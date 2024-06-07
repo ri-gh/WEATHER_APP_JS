29157315b1af6c39233fce2a5b22771203374e77
Richard Gheesah <richard.gheesah@interieur.gouv.fr>
2024-06-07 09:20:51 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,119 @@
+// source: https://youtu.be/VaDUGPMjzOM
+const weatherForm = document.querySelector('.weatherForm');
+const cityInput = document.querySelector('.cityInput');
+const card = document.querySelector('.card');
+// getApiKey() is rendering the API key in an env.js file
+let apiKey = getApiKey();
+// const apiKey = 'process.env.API_KEY' if using dotenv
+
+weatherForm.addEventListener('submit', async event => {
+
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if(city){
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error){
+            console.error(error)
+            displayError(error)
+        }
+    }
+    else{
+        displayError("Please enter a city")
+    }
+})
+
+async function getWeatherData(city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+    const response = await fetch(apiUrl)
+
+    if(!response.ok){
+        throw new Error('Could not fetch weather data')
+    }
+
+    return await response.json()
+}
+
+function displayWeatherInfo(data){
+    //on destructure les objets et les array d'objets
+    const { name: city,
+            main: {temp,humidity},
+            weather:[{description,id}]} = data
+    
+    // on reset le contenu de la classe .card
+    card.textContent = "";
+    card.style.display= "flex"
+    
+    // on crée des éléments dans le DOM
+    const cityDisplay = document.createElement("h1")
+    const tempDisplay = document.createElement("p")
+    const humidityDisplay = document.createElement("p")
+    const descDisplay = document.createElement("p")
+    const weatherEmoji = document.createElement("p")
+    
+    // on ajoute le texte à l'élément html
+    cityDisplay.textContent = city
+    tempDisplay.textContent = `${(temp - 273.15).toFixed(1) }°C` //pour avoir la temp de Kalvin à Celsius et arrondir à 1 chiffre
+    humidityDisplay.textContent = `Humidity: ${humidity}%`
+    descDisplay.textContent = description
+    weatherEmoji.textContent = getWeatherEmoji(id)
+
+    //on ajoute l'élément à une classe du fichier css
+    cityDisplay.classList.add('cityDisplay')
+    tempDisplay.classList.add('tempDisplay')
+    humidityDisplay.classList.add('humidityDisplay')
+    descDisplay.classList.add('descDisplay')
+    weatherEmoji.classList.add('weatherEmoji')
+
+    //on append l'element à l'élément de la classe 'card'
+    card.appendChild(cityDisplay)
+    card.appendChild(tempDisplay)
+    card.appendChild(humidityDisplay)
+    card.appendChild(descDisplay)
+    card.appendChild(weatherEmoji)
+
+}
+
+function getWeatherEmoji(weatherId){
+    //voir les id codes sur le site de openweatherapi (35 min' du tuto)
+    //touche 'Windows' + ';' pour faire apparaitre les emojis
+    //le switch vérifie la condition true pour chacun des cas exposés
+    //source https://youtu.be/z2fcWdoph4U
+    switch(true){
+            case (weatherId >= 200 && weatherId < 300):
+                return "⛈️";
+            case (weatherId >= 300 && weatherId < 400):
+                return "🌧️";
+            case (weatherId >= 500 && weatherId < 600):
+                return "🌧️";  
+            case (weatherId >= 600 && weatherId < 700):
+                return "❄️";    
+            case (weatherId >= 700 && weatherId < 800):
+                return "🌫️";
+            case (weatherId === 800):
+                return "☀️";  
+            case (weatherId >= 801 && weatherId < 810):
+                return "☁️";  
+            default:
+                return "❓"
+            }
+    }
+
+function displayError(message){
+    const errorDisplay = document.createElement("p")
+    errorDisplay.textContent = message
+    // on ajoute à la classe 'errorDisplay' du css
+    errorDisplay.classList.add("errorDisplay")
+
+    // on réinitialise le contenu de la classe card
+    card.textContent = "";
+    card.style.display = "flex"
+    // on ajoute un element enfant à la classe card
+    card.appendChild(errorDisplay)
+
+}
